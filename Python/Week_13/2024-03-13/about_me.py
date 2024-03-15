bfc34032dfbd3af719464ab283b6881aa46e3669
Robert <rbaez39@bostonk12.org>
2024-03-15 09:53:18 -0400

    GH-2024-03-14

---
+++ Python/Week_13/2024-03-13/about_me.py
@@ -0,0 +1,7 @@
+# My name is robert
+# 3-15-2024
+
+print ("The city i was born in was santo domingo.")
+print ("The country i was born in was dominican republic.")
+print ("hello im Robert I am 14 years old.")
+print ("Hello my favorite academic class is ELA because i get to write and work with friends.")
